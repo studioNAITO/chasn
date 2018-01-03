b52368014acb5a7e962311a55fe5ee413c3a384c
archxiao <ayates@hnerv.com>
2018-01-03 02:47:58 -0800

    v1.02
    Fix for downloading binaries and `echos`

--- chasn.sh
+++ chasn.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 dldir=$(pwd)
-echo $dldir
 
 echo "chasn v1.0\n"
 if [ "$1" = "install" ]
@@ -24,18 +23,12 @@ then
     exit 1;
   fi
 
-  if [ -z "$2" ]
-  then
-    echo "chasn install [dl/'directory path']"
-    exit 1;
-  fi
-
 
   # bashrc
   echo 'export chasn=/usr/local/chasn #chasn' >> ~/.bashrc
 
-  echo 'export ANDROID_HOME=$chasn #chasn' >> ~/.bashrc
-  echo 'export ANDROID_SWT=$chasn/tools/lib/arm #chasn' >> ~/.bashrc
+  echo "export ANDROID_HOME=$chasn #chasn" >> ~/.bashrc
+  echo "export ANDROID_SWT=$chasn/tools/lib/arm #chasn" >> ~/.bashrc
   echo 'export NDK_KNOWN_ABIS="armeabi-v7a armeabi-v7a-hard armeabi" #chasn' >> ~/.bashrc
   echo 'export NDK_KNOWN_ARCHS="arm x86" #chasn' >> ~/.bashrc
   echo 'export HOST_ARCH="arm" #chasn' >> ~/.bashrc
@@ -45,37 +38,42 @@ then
   echo "alias android='$chasn/tools/android' #chasn" >> ~/.bashrc
   echo "alias ant='$chasn/ant/bin/ant' #chasn" >> ~/.bashrc
 
+  touch $chasn/gcokie
+
   # binaries
-  if [ "$2" = "dl" ]
-  then
-    ggURL='https://drive.google.com/uc?export=download'  
-    getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
+  ggURL='https://drive.google.com/uc?export=download'  
 
-    echo "DOWNLOADING: ant"
-    ggID='0B3WqEY74PP5AS0ducmFxMlBNSUk' # ant
-    curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "ant.bzip2"
+  echo "DOWNLOADING: ant"
+  ggID='0B3WqEY74PP5AS0ducmFxMlBNSUk' # ant
+  filename="$(curl -sc $chasn/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+  getcode="$(awk '/_warning_/ {print $NF}' $chasn/gcokie)"  
+  curl -Lb $chasn/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "ant.bzip2"
 
-    echo "DOWNLOADING: arm-tools"
-    ggID='0B3WqEY74PP5AZ3E5ekR4cTZCNFE' # arm-tools
-    curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "arm-tools.bzip2"
+  echo "DOWNLOADING: arm-tools"
+  ggID='0B3WqEY74PP5AZ3E5ekR4cTZCNFE' # arm-tools
+  filename="$(curl -sc $chasn/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+  getcode="$(awk '/_warning_/ {print $NF}' $chasn/gcokie)"  
+  curl -Lb $chasn/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "arm-tools.bzip2"
 
-    echo "DOWNLOADING: ndk"
-    ggID='0B3WqEY74PP5AZ3ZjVjRjVWZObVE' # ndk
-    curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "ndk.bzip2"
-  else
-    echo "EXTRACTING: ant"
-    tar -xjf $2/ant.bzip2
-    echo "EXTRACTED: ant\nEXTRACTING: arm-tools"
-    tar -xjf $2/arm-tools.bzip2 && mv arm-build-tools tools;
-    echo "EXTRACTED: arm-tools\nEXTRACTING: ndk"
-    tar -xjf $2/ndk.bzip2
-    echo "EXTRACTED: ndk"
-  fi
+  echo "DOWNLOADING: ndk"
+  ggID='0B3WqEY74PP5AZ3ZjVjRjVWZObVE' # ndk
+  filename="$(curl -sc $chasn/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+  getcode="$(awk '/_warning_/ {print $NF}' $chasn/gcokie)"  
+  curl -Lb $chasn/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "ndk.bzip2"
 
   ln -s $chasn/tools $chasn/build-tools/25.0.2
 
-  echo "\nINSTALLED: binaries\n"
-
+  echo "DOWNLOADED: binaries"
+  
+  echo "EXTRACTING: ant"
+  tar -xjf $chasn/ant.bzip2
+  echo "EXTRACTED: ant"
+  echo "EXTRACTING: arm-tools"
+  tar -xjf $chasn/arm-tools.bzip2 && mv arm-build-tools tools;
+  echo "EXTRACTED: arm-tools"
+  echo "EXTRACTING: ndk"
+  tar -xjf $chasn/ndk.bzip2
+  echo "EXTRACTED: ndk"
 
   #current shell
   export chasn=/usr/local/chasn
@@ -86,18 +84,18 @@ then
   export NDK_KNOWN_ARCHS="arm x86"
   export HOST_ARCH="arm"
 
-  alias chasn='sh $chasn/chasn.sh'
-  alias sdkmanager='$chasn/tools/bin/sdkmanager --sdk_root=$chasn/'
-  alias android='$chasn/tools/android'
-  alias ant='$chasn/ant/bin/ant' #chasn
+  alias chasn="sh $chasn/chasn.sh"
+  alias sdkmanager="$chasn/tools/bin/sdkmanager --sdk_root=$chasn/"
+  alias android="$chasn/tools/android"
+  alias ant="$chasn/ant/bin/ant" #chasn
 
   export PATH=$PATH:$chasn/ant/bin
 
   echo "INSTALLED: bashrc\n"
 
-  echo "SETUP: platform-tools\n"
-  sdkmanager "platform-tools";
-  echo "SETUP: complete\n"
+  #echo "SETUP: platform-tools\n"
+  #sdkmanager "platform-tools";
+  #echo "SETUP: complete\n"
 
   exit 0;
 fi
